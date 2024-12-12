a61bbeaa2236258d4705c27ec9732da6663c1d95
palak <kaurpalakpreet6@gmail.com>
2024-12-12 17:41:14 +0530

    welcome screeen

--- src/compnents/Welcome.jsx
+++ src/compnents/Welcome.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function Welcome() {
   return (
-    <div className="bg-purple-100 rounded-lg shadow-md p-6 flex justify-between items-center">
+    <div className="bg-purple-100 rounded-lg shadow-md p-6 flex justify-start items-center">
       <div>
         <h1 className="text-2xl font-bold text-purple-900">Hello Palak👋</h1>
         <p className="text-sm text-purple-700">You can manage your things from here</p>
       </div>
-      <div className="ml-4 text-purple-900 text-4xl font-bold">
-        Welcome
+      <div className="ml-52 text-purple-900 text-6xl font-extrabold">
+      <span className="font-serif italic">Welcome</span>
       </div>
     </div>
   );
 }
+
+
